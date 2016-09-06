fb8692844209ab055dcf23efd442ecfac7c5de68
Kevin Scannell <kscanne@gmail.com>
2016-09-06 14:31:37 -0500

    more examples

--- transition/refsetc.cpp
+++ transition/refsetc.cpp
@@ -11,24 +11,40 @@ int main() {
 
   RichPoint p(0,0);
   RichPoint q(1,1);
+  RichPoint r(2,2);
   cout << "p = " << p << endl;
   cout << "q = " << q << endl;
+  cout << "r = " << r << endl;
 
   RichPoint* pptr = &p;
   RichPoint* qptr = &q;
+  RichPoint* rptr = &r;
   cout << "pptr = " << pptr << endl;
   cout << "qptr = " << qptr << endl;
+  cout << "rptr = " << rptr << endl;
 
   RichPoint& pref = p;
   RichPoint& qref = q;
+  RichPoint& rref = r;
   cout << "pref = " << pref << endl;
   cout << "qref = " << qref << endl;
+  cout << "rref = " << rref << endl;
 
+  cout << "and now bitwise copy q to p" << endl;
   p = q;
   cout << "p = " << p << endl;
   cout << "q = " << q << endl;
   cout << "pptr = " << &p << endl;
   cout << "qptr = " << &q << endl;
+
+  cout << "and now copy via reference, not like Python!" << endl;
+  qref = rref;
+  cout << "q = " << q << endl;
+  cout << "r = " << r << endl;
+  cout << "qref = " << qref << endl;
+  cout << "rref = " << rref << endl;
+  cout << "qptr = " << &q << endl;
+  cout << "rptr = " << &r << endl;
 
   cout << "p, scaled to unit length: ";
   printScaled(p);
