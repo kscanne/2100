cb331b4f831ed1199b949552049facc3e9a8b1c5
Kevin Scannell <kscanne@gmail.com>
2016-10-05 19:38:39 -0500

    tweaks

--- lab05/lab05.cpp
+++ lab05/lab05.cpp
@@ -40,14 +40,18 @@ void print_tree_by_level(BinNode<E>* root) {
 int main()
 {
   BSTNode<int,int>* root = new BSTNode<int,int>(1,1);
-  root->setLeft(new BSTNode<int,int>(2,2));
-  root->setRight(new BSTNode<int,int>(3,3));
+  BSTNode<int,int>* left = new BSTNode<int,int>(2,2);
+  BSTNode<int,int>* right = new BSTNode<int,int>(3,3);
+  left->setLeft(new BSTNode<int,int>(4,4));
+  left->setRight(new BSTNode<int,int>(5,5));
+  root->setLeft(left);
+  root->setRight(right);
   print_tree(root);
   cout << " Node count = " << count(root) << endl;
   cout << " Height = " << height(root) << endl;
   cout << " Leaf count = " << leaf_count(root) << endl;
   cout << " Sum of node values = " << sum_nodes(root) << endl;
-  for (int i=1; i <= 4; i++) {
+  for (int i=0; i <= 6; i++) {
     if (search(root, i)) {
       cout << "Found " << i << " in the tree..." << endl;
     }
