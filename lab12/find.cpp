fc62f360e0589781d121063a90ecb0be8cc8183e
Kevin Scannell <kscanne@gmail.com>
2016-11-30 08:03:17 -0600

    for KPSJ

--- lab12/find.cpp
+++ lab12/find.cpp
@@ -3,6 +3,9 @@
 #include <string.h>
 using namespace std;
 
+// this implementation only tells us if the substring is there or not
+// We could store indices at the *interior nodes* of the trie if
+// we wanted to return the index of the first place where the substring occurs
 bool findSubstring(const char* big, const char* small) {
   Trie t;
   int biglen = strlen(big);
@@ -12,23 +15,27 @@ bool findSubstring(const char* big, const char* small) {
   return t.longestPrefix(small) == strlen(small);
 }
 
+void findUI(const char* b, const char* s) {
+  if (findSubstring(b,s)) {
+    cout << "String contains \"" << s << "\"!\n";
+  }
+  else {
+    cout << "String does not contain \"" << s << "\"!\n";
+  }
+}
+
 int main() {
 
   char b[128];
-  char s[128];
 
   cout << "Big string to search in: ";
-  cin >> b;
-  cout << "Smaller string to search for: ";
-  cin >> s;
+  cin.getline(b,sizeof(b));
 
-  if (findSubstring(b,s)) {
-    cout << "Found it!\n";
-  }
-  else {
-    cout << "Not found.\n";
-  }
+  // the typical setup is that you want to search for many short strings
+  // in one long string, so you can just build the trie once and then
+  // search very quickly thereafter; we're not doing that here
+  findUI(b, "plate");
+  findUI(b, "shrimp");
 
   return 0;
-
 }
