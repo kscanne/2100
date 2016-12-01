46234a64e88ec7831312180b7d26ea0ea9c35417
Kevin Scannell <kscanne@gmail.com>
2016-12-01 09:55:57 -0600

    golf

--- lab12/stab.cpp
+++ lab12/stab.cpp
@@ -7,20 +7,16 @@ using namespace std;
 int main() {
 
   Trie t;
-  char s[5000];
+  char s[5000], s2[5000];
   ifstream i("mobydick.txt");
   i.read(s, sizeof(s));
   int slen = strlen(s);
-  for (int i=0; i < slen; i++) {
+  for (int i=0; i < slen; i++)
     t.insert(s+i,i);
-  }
 
-  char s2[5000];
   ifstream i2("startrek.txt");
   i2.read(s2, sizeof(s2));
-  int s2len = strlen(s2);
-  int bestindex = -1;
-  int bestlen = 0;
+  int s2len = strlen(s2), bestindex=-1, bestlen=0;
   for (int i=0; i < s2len; i++) { 
     int temp = t.longestPrefix(s2+i);
     if (temp > bestlen) {
@@ -30,9 +26,6 @@ int main() {
   }
   s2[bestindex+bestlen] = 0;
 
-  cout << "Longest common substring:\n";
-  cout << s2+bestindex << "\n";
-
+  cout << "Longest common substring:\n" << s2+bestindex << "\n";
   return 0;
-
 }
